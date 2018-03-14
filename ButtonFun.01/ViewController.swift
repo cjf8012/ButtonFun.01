ef72b49705386a910b2a1990dee2e19db2bf793b
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-03-14 12:43:16 +0900

    add view background color

--- ButtonFun.01/ViewController.swift
+++ ButtonFun.01/ViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.yellow
-    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPrdssed(_ sender: Any) {
+        if flag == 0 {
+            view.backgroundColor = UIColor.green
+            flag = 1 }
+        else if flag == 1 {
+            view.backgroundColor = UIColor.blue
+            flag = 0 }
+        }
     }
-
-
-}
 
